455226b0e0eb2a80850f751909ea4849a01d41b1
Roboto7 <cyberlordroboto@gmail.com>
2025-10-26 13:03:56 +0100

    support for both number and string

--- src/helper/global.ts
+++ src/helper/global.ts
@@ -1,9 +1,10 @@
-export const formatMonetaryValue = (value: number) => {
-    const formatter = new Intl.NumberFormat('en-NG', {
-      style: 'currency',
-      currency: 'NGN',
-    });
-    return formatter.format(value);
+export const formatMonetaryValue = (value: number | string) => {
+  const numericValue = typeof value === 'string' ? parseFloat(value) : value;
+  const formatter = new Intl.NumberFormat('en-NG', {
+    style: 'currency',
+    currency: 'NGN',
+  });
+  return formatter.format(numericValue);
   };
 
- export const capitalizeFirstLetter = (word:string) => word.charAt(0).toUpperCase() + word.slice(1);
+export const capitalizeFirstLetter = (word:string) => word.charAt(0).toUpperCase() + word.slice(1);
